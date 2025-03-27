95844f07b5ca049c9a3bc071975a31f190bdb298
Ayush Sharma <ayushsharma4999@gmail.com>
2025-03-27 21:45:52 +0530

    Data ingestion

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -25,6 +25,8 @@ class DataIngestion:
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
             df.to_csv(self.ingestion_config.test_data_path,index=False,header = True)
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header = True)
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
             logging.info("Data Ingestion is completed")
             return(
                 self.ingestion_config.train_data_path,
