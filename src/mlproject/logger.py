f21aa3423599c565358056b4174ca4a866dd0fa4
Ayush Sharma <ayushsharma4999@gmail.com>
2025-03-27 21:42:05 +0530

    Data ingestion

--- src/mlproject/logger.py
+++ src/mlproject/logger.py
@@ -2,14 +2,19 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(log_path,exist_ok=True)
+# Define log directory
+log_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_dir, exist_ok=True)  # Create only the directory
 
-LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+# Define log file path
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
+
+# Configure logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-    )
+)
 
+logging.info("Logging setup complete.")  # Test log entry
